909bbf88eb0faa29874f24baa01d6962f47c8b08
qy <jimmy@ffglobaltech.com>
2025-07-01 12:29:39 +0800

    update

--- postgres/model.go
+++ postgres/model.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// only record each day RTP , and compute total RTP on each day 00:00 and store it to redis
 type RoomRtp struct {
 	ID    primitive.ObjectID
 	Time  time.Time `bson:"date"`
@@ -19,4 +20,3 @@ type GameRtp struct {
 	Bet   uint64
 	Score uint64
 }
-
